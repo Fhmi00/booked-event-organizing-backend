2e16f2f6b7f1c8ac5acd3328b1b42367b4964a71
Fhmi00 <anisfahmi045@gmail.com>
2022-09-03 15:27:02 +0700

    add: table event

---
+++ src/controllers/event.js
@@ -0,0 +1,79 @@
+const eventModel = require("../models/event");
+const wrapper = require("../utils/wrapper");
+
+module.exports = {
+  getAllEvent: async (request, response) => {
+    try {
+      //   console.log(request.query);
+      const result = await eventModel.getAllEvent();
+      return wrapper.response(
+        response,
+        result.status,
+        "Success Get Data !",
+        result.data
+      );
+    } catch (error) {
+      const {
+        status = 500,
+        statusText = "Internal Server Error",
+        error: errorData = null,
+      } = error;
+      return wrapper.response(response, status, statusText, errorData);
+    }
+  },
+  getEventById: async (request, response) => {
+    try {
+      const { id } = request.params;
+      const result = await eventModel.getEventById(id);
+
+      if (result.data.length < 1) {
+        return wrapper.response(
+          response,
+          404,
+          `Data By Id ${id} Not Found`,
+          []
+        );
+      }
+
+      return wrapper.response(
+        response,
+        result.status,
+        "Success Get Data By Id",
+        result.data
+      );
+    } catch (error) {
+      const {
+        status = 500,
+        statusText = "Internal Server Error",
+        error: errorData = null,
+      } = error;
+      return wrapper.response(response, status, statusText, errorData);
+    }
+  },
+  createEvent: async (request, response) => {
+    try {
+      console.log(request.body);
+      const { name, price } = request.body;
+      const setData = {
+        name,
+        price,
+      };
+
+      const result = await eventModel.createEvent(setData);
+
+      return wrapper.response(
+        response,
+        result.status,
+        "Success Create Data",
+        result.data
+      );
+    } catch (error) {
+      const {
+        status = 500,
+        statusText = "Internal Server Error",
+        error: errorData = null,
+      } = error;
+      return wrapper.response(response, status, statusText, errorData);
+    }
+  },
+};
---
+++ src/models/event.js
@@ -0,0 +1,45 @@
+const supabase = require("../config/supabase");
+
+module.exports = {
+  getAllEvent: () =>
+    new Promise((resolve, reject) => {
+      supabase
+        .from("event")
+        .select("*")
+        .then((result) => {
+          if (!result.error) {
+            resolve(result);
+          } else {
+            reject(result);
+          }
+        });
+    }),
+  getEventById: (id) =>
+    new Promise((resolve, reject) => {
+      // SELECT * FROM product WHERE id = "123"
+      supabase
+        .from("event")
+        .select("*")
+        .eq("id", id)
+        .then((result) => {
+          if (!result.error) {
+            resolve(result);
+          } else {
+            reject(result);
+          }
+        });
+    }),
+  createEvent: (data) =>
+    new Promise((resolve, reject) => {
+      supabase
+        .from("event")
+        .insert([data]) // insert([{name: "Tea", price: 5000}])
+        .then((result) => {
+          if (!result.error) {
+            resolve(result);
+          } else {
+            reject(result);
+          }
+        });
+    }),
+};
