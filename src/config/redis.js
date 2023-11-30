8f0ae1694aaa9d086ea6c3d110a560fb6ea00daa
Fhmi00 <anisfahmi045@gmail.com>
2023-11-30 13:19:57 +0700

    update redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,8 @@
 const redis = require("redis");
 
 const redisPassword = "LjR8gfpnHd1ScFkyv7dycfMeLh1uiuQa";
-const redisHost = "redis-11026.c270.us-east-1-3.ec2.cloud.redislabs.com";
-const redisPort = "11026";
+const redisHost = "redis-10306.c274.us-east-1-3.ec2.cloud.redislabs.com";
+const redisPort = "10306";
 
 const client = redis.createClient({
   socket: {
