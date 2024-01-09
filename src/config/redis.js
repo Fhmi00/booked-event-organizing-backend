d8070b39cd8efbd0c592e47678bcfab9c03e601f
Fhmi00 <anisfahmi045@gmail.com>
2024-01-09 07:15:12 +0700

    feat: redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,8 @@
 const redis = require("redis");
 
-const redisPassword = "Ym8e7y7tbCdtlaFRQGv4E7YFDXUgRRFo";
-const redisHost = "redis-10306.c274.us-east-1-3.ec2.cloud.redislabs.com";
-const redisPort = "10306";
+const redisPassword = "u0dWuJZtbl2DbTDhJA4M8bfmMqN8sULk";
+const redisHost = "redis-11470.c326.us-east-1-3.ec2.cloud.redislabs.com";
+const redisPort = "11470";
 
 const client = redis.createClient({
   socket: {
