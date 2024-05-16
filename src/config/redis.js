2c2cfe01ba635ddb296806fc1abee36928fc08fd
Fhmi00 <anisfahmi045@gmail.com>
2024-05-16 19:06:28 +0700

    fix redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,9 +1,9 @@
 const redis = require("redis");
-require("dotenv").config();
+// require("dotenv").config();
 
-const redisPassword = process.env.REDIS_PASSWORD;
-const redisHost = process.env.REDIS_HOST;
-const redisPort = process.env.REDIS_PORT;
+const redisPassword = "Wfg5WGJGJKAvImG2gk7LwYtd2ISd5hGh";
+const redisHost = "redis-10638.c261.us-east-1-4.ec2.redns.redis-cloud.com";
+const redisPort = "10638";
 
 const client = redis.createClient({
   socket: {
@@ -18,7 +18,8 @@ const client = redis.createClient({
   client.on("connect", () => {
     // eslint-disable-next-line no-console
     console.log("You're connected db redis ...");
-    console.log(redisHost);
+    // console.log(redisHost);
+    // console.log(redisPort);
   });
 })();
 
