5bbb56c4066c1e598695275fcb9774089643b1c1
Fhmi00 <anisfahmi045@gmail.com>
2024-07-03 17:30:51 +0700

    fix redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,9 +1,9 @@
 const redis = require("redis");
 // require("dotenv").config();
 
-const redisPassword = "Wfg5WGJGJKAvImG2gk7LwYtd2ISd5hGh";
-const redisHost = "redis-10638.c261.us-east-1-4.ec2.redns.redis-cloud.com";
-const redisPort = "10638";
+const redisPassword = "AUIy6RsY3v4orDjN9RuZ52mMZtfUSSbf";
+const redisHost = "redis-16520.c245.us-east-1-3.ec2.redns.redis-cloud.com";
+const redisPort = "16520";
 
 const client = redis.createClient({
   socket: {
